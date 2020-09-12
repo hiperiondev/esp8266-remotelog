72b9717d7bd213493f1a4dd093854658a9434954
egonzalez <egonzalez@msi.notebook.hiperion.local>
2020-09-12 13:30:54 -0300

    Add project header

---
+++ isc_project.h
@@ -0,0 +1,23 @@
+/*
+ * This file is part of the esp-iot-secure-core distribution (https://github.com/hiperiondev/esp-iot-secure-core).
+ * Copyright (c) 2020 Emiliano Augusto Gonzalez (comercial@hiperion.com.ar)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef _ISC_PROJECT_
+#define _ISC_PROJECT_
+
+#define ISC_CORE_REMOTELOG_PORT   23
+#define ISC_CORE_REMOTELOG_TOUT   30
+
+#endif
